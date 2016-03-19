823832a9f49f42136b5e7176a515eb7ad40d3941
Randall2994 <randall291094@hotmail.com>
2016-03-18 18:53:13 -0600

    Se declaran las funciones para obtener los datos

--- Funciones.c
+++ Funciones.c
@@ -1,11 +1,14 @@
 include "Alien_t.h"
 include <string.h>
 
+//Funciones que guardan los datos//
+void alien add_Alien(alien *lista ,char nombre, int id, char especie);
+void add_Planeta(planetas *lista, char nombre, int code, char agua, char oxigen);
+void add_idioma(idioma *lista, char nombre, char tipo, int simbolos);
+//Funcion para imprimir datos//
+void get_Datos(alien*listaA,planeta*listaP,idioma*listaI);
 
-void alien add_Alien(char nombre, int id, char especie);
-void add_Planeta(char nombre, int code, char agua, char oxigen);
-void add_idioma(char nombre, char tipo, int simbolos);
-
+//Declaracion de las funciones//
 void add_Alien(alien *lista, char name, int ID, char esp){
 	
 	lista = malloc(sizeof(alien));
@@ -15,7 +18,6 @@ void add_Alien(alien *lista, char name, int ID, char esp){
 	strcpy(lista->especie, esp);
 
 }
-
 
 void add_Planeta(planetas *lista, char name, int code, char water, char oxigen){
 
@@ -28,7 +30,6 @@ void add_Planeta(planetas *lista, char name, int code, char water, char oxigen){
 	
 }
 
-
 void add_idioma(idioma *lista, char name, char type, int symbols){
 
 	lista = malloc(sizeof(idioma));
@@ -37,5 +38,25 @@ void add_idioma(idioma *lista, char name, char type, int symbols){
 	lista->simbolos = symbols;
 
 }
+
+void get_Datos(alien*listaA,planeta*listaP,idioma*listaI){
+	printf("%s\n",listaA->nombre);
+	printf("%d\n",listaA->id);
+	printf("%s\n",listaA->especie);
+
+	printf("%s\n",listaP->nombre);
+	printf("%d\n",listaP->star_wars_code);
+	printf("%c\n",listaP->agua);
+	printf("%c\n",listaP->oxigeno);
+
+	printf("%d\n",listaI->nombre);
+	printf("%c\n",listaI->tipo);
+	printf("%d\n",listaI->simbolos);
+}
+
+
+
+
+
 
 
