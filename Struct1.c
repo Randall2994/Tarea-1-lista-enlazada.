36aee48f5d3690737bb786fc1205aa9e6c9cfaff
Randall2994 <randall291094@hotmail.com>
2016-03-18 18:56:28 -0600

    Borrar pruebas pasadas

--- Struct1.c
+++ Struct1.c
@@ -4,15 +4,7 @@
 #include"Alien_t.h"
 
 main(){
-	alien *lista_aliens;	
-	lista_aliens=malloc(sizeof(alien));	
-	strcpy(lista_aliens->nombre,"Alien1:Superman");
-	lista_aliens->id=40;
-	
 
-	printf("%s\n",lista_aliens->nombre);
-	printf("%d\n",lista_aliens->id);
-	printf("%s\n",lista_aliens->especie);	
 return 1;
 }
 
