1ca1b8bbb2d7a893ce939c08ec2ffd57a3767481
Randall2994 <randall291094@hotmail.com>
2016-04-01 22:07:52 -0600

    Se pasan funciones al main

---
+++ Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include"Funciones4.c"
+
+main(){
+    
+    
+    insertar_A_O(root);
+    insertar_A_O(root);
+    insertar_A_O(root);
+    insertar_A_O(root);
+    imp_A();
+}
