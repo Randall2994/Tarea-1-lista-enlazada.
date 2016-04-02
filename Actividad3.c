8bac5fda41c7ec0a6b75a41c80c094e33de4567a
Randall2994 <randall291094@hotmail.com>
2016-04-01 22:16:11 -0600

    Se agrega Archivo

--- Actividad3.c
+++ Actividad3.c
@@ -1,66 +1,110 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Instituto Tecnologico de Costa Rica - Sede Central
+Escuela de Computacion - Estructuras de Datos
+Profesor: Esteban Arias
+Actividad 3
 
-/* 
- * File:   main.c
- * Author: randall
- *
- * Created on March 13, 2016, 12:00 PM
- */
+Alejandro Pacheco Quesada - 2015171007
+Randall Moya Madrigal - 2015183025
 
-#include <stdio.h>
-#include <stdlib.h>
+Fecha de entrega: 18-3-16
+I Semestre, 2016
+
+*/
+
+
+#include<stdio.h>
 #include<string.h>
-#include "Structs.h"
+#include<stdlib.h>
+#include"Alien_t.h"
+
+alien * add_Alien(alien *lista, const char *name, int ID, const char *esp);
+planetas * add_Planeta(planetas *lista, const char *name, int code, char water, char oxigen);
+idioma * add_idioma(idioma *lista, const char *name, char type, int symbols);
+void get_Datos(alien*listaA,planetas*listaP,idioma*listaI);
+
+int main(){
+	
+	alien *lista_alien;
+	planetas *lista_planetas;
+	idioma *lista_idiomas;
+	
+
+	lista_alien = add_Alien(lista_alien, "Superman", 80, "Kryptonian");
+	lista_planetas = add_Planeta(lista_planetas, "Krypton", 78452, 'Y', 'N');
+	lista_idiomas = add_idioma(lista_idiomas, "Ingles", '9', 26);
 
 
-typedef struct nodo_t{
-    int valor;
-    struct nodo_t *siguiente;
-}nodo;
+	get_Datos(lista_alien, lista_planetas, lista_idiomas);
 
-void insertar(nodo *h, int v,);
-void printlista(nodo *h);
+	
+	lista_alien->planetas = lista_planetas;
+	lista_alien->idiomas = lista_idiomas;
 
-int main() {
+	printf("Idioma que habla el alien: %s\n", lista_alien->idiomas->nombre);
+	printf("Planeta donde vive el alien: %s\n", lista_alien->planetas->nombre);
 
-    
-    nodo *head = malloc(sizeof(nodo));
-    head->valor = 8;
-    insertar(head, 4);
-    
-    insertar(head, 1);
-    
-    printlista(head);
+return 1;
 }
 
-void insertar(nodo *h, int v){
-    if(h == NULL){
-        h = malloc(sizeof(nodo));
-        h->valor = v;
-        
-    }
-    else{
-        nodo *tmp;
-        tmp = h;
-        while(tmp->siguiente!=NULL){
-            tmp = tmp->siguiente;
-        }
-        tmp->siguiente = malloc(sizeof(nodo));
-        tmp->siguiente->valor = v;
-        
-    }
+//Declaracion de las funciones//
+alien * add_Alien(alien *lista, const char *name, int ID, const char *esp){
+	
+	lista = malloc(sizeof(alien));
+	strcpy(lista->nombre, name);
+	lista->id = ID;
+	lista->especie = malloc(strlen(esp));
+	strcpy(lista->especie, esp);
+
+	return lista;
 
 }
 
-void printlista(nodo *h){
-    nodo *tmp;
-    tmp = h;
-    while(tmp!=NULL){
-        printf("Nodo: %d \n", tmp->valor);
-        tmp = tmp->siguiente;
-    }
+planetas * add_Planeta(planetas *lista, const char *name, int code, char water, char oxigen){
+
+	lista = malloc(sizeof(planetas));
+	strcpy(lista->nombre, name);
+	lista->star_wars_code = code;
+	lista->agua = water;
+	lista->oxigeno = oxigen;
+
+	return lista;
+	
+	
 }
+
+idioma * add_idioma(idioma *lista, const char *name, char type, int symbols){
+
+	lista = malloc(sizeof(idioma));
+	strcpy(lista->nombre, name);
+	lista->tipo = type;
+	lista->simbolos = symbols;
+
+	return lista;
+}
+
+
+void get_Datos(alien*listaA,planetas*listaP,idioma*listaI){
+	printf("Alien: \n");
+	printf("Nombre: %s\n",listaA->nombre);
+	printf("ID: %d\n",listaA->id);
+	printf("Especie: %s\n",listaA->especie);
+	printf("\n");
+	
+	printf("Planeta: \n");
+	printf("Nombre: %s\n",listaP->nombre);
+	printf("Star wars code: %d\n",listaP->star_wars_code);
+	printf("Agua?: %c\n",listaP->agua);
+	printf("Oxigeno?: %c\n",listaP->oxigeno);
+	printf("\n");
+
+	printf("Idioma: \n");
+	printf("Nombre: %s\n",listaI->nombre);
+	printf("Tipo: %c\n",listaI->tipo);
+	printf("Simbolos: %d\n",listaI->simbolos);
+	printf("\n");
+	
+}
+
+
+
